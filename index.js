17b83874b982f3619f2f42a2c3e13a6613c7d532
Divya <divya.raphy@dci-student.org>
2025-05-02 01:24:17 +0200

    server-de

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ let storage = [
 ];
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); 
+  res.header('Access-Control-Allow-Origin', 'https://server-f92x.onrender.com'); 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
